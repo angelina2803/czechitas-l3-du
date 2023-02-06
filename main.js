a613595e230e383cb88d5997d4cb1e89c1f5a08f
Angelina Yumatova <>
2023-02-06 14:05:03 +0600

    change

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-// tady je místo pro náš program
+// 1 ukol 
+
+let Jmeno = prompt("Jake je vase jmeno");
+let Primeni = prompt("Jake je vase primeni?");
+let rok = Number(prompt("V jakém roce jste se narodili?"));
+
+let vek = 2023 - rok;
+
+let vysledek = document.querySelector('p');
+// vysledek.innerHTML = Jmeno + " "+ Primeni + " "+ vek + " "+ barva;
+vysledek.innerHTML =  Jmeno + " "+ Primeni + " "+ vek;
+
+//2 ukol
+
+let vysledek2 = document.querySelector('.vystupProgramuBarva');
+let Barva = prompt("Jake je vase oblíbena barva (anglicky)");
+vysledek2.style.backgroundColor = Barva;
+vysledek2.innerHTML = Barva;
+
+
 
 
 
